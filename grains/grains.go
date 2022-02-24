7ef35636a6702142e31f8fee00229befa149da7e
Dhanush reddy <Dhanushreddy09@users.noreply.github.com>
2022-02-24 22:16:59 +0530

    added unit tests to grains

--- grains/grains.go
+++ grains/grains.go
@@ -2,18 +2,19 @@ package grains
 
 import (
 	"errors"
+	"math"
 )
 
 var sum uint64
 
 func Square(number int) (uint64, error) {
-	if number <= 0 || number > 64 {
+	if number < 0 || number > 64 {
 		return 0, errors.New("should not be less than zero")
 	}
-	ans := 1 << (number - 1)
+	ans := 1 << (number)
 	return uint64(ans), nil
 }
 
 func Total() uint64 {
-	return uint64((1 << (64)) - 1)
+	return math.MaxUint64
 }
---
+++ grains/grains_test.go
@@ -0,0 +1,28 @@
+package grains
+
+import "testing"
+
+func benchmarkSquare(i int, b *testing.B) {
+	for n := 0; i < b.N; n++ {
+		Square(i)
+	}
+}
+
+func testSquare(t *testing.T, i int, expected uint64) {
+	actual, err := Square(i)
+	if err != nil {
+		t.Error(err)
+	}
+	if actual != expected {
+		t.Errorf("Square(%d) should be %d but was %d", i, expected, actual)
+	}
+}
+
+func TestSquare(t *testing.T) {
+	testSquare(t, 2, 4)
+}
+
+func BenchmarkSquare1(b *testing.B)  { benchmarkSquare(1, b) }
+func BenchmarkSquare2(b *testing.B)  { benchmarkSquare(2, b) }
+func BenchmarkSquare10(b *testing.B) { benchmarkSquare(10, b) }
+func BenchmarkSquare20(b *testing.B) { benchmarkSquare(20, b) }
